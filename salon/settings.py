ccd6df65dcd2cce928eb890dc9ee7aae3a95cc37
Nyakinyua <55144168+Nyakinyua@users.noreply.github.com>
2021-03-05 14:35:20 +0300

    Update settings.py
    Edit Email Verification to none

--- salon/settings.py
+++ salon/settings.py
@@ -140,7 +140,7 @@ UPLOADCARE = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
